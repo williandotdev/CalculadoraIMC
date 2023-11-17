c3420b02ea551dcfde2400b9231f8506b2c9620a
Willian Ferreira <willian.dev.ferreira@gmail.com>
2023-11-17 16:26:02 -0300

    Delete alert-error.js

--- alert-error.js
+++
@@ -1,9 +0,0 @@
-export const AlertError = {
-    element: document.querySelector('.alert-error'),
-    open(){
-        this.element.classList.add('open')
-    },
-    close(){
-        this.element.classList.remove('open')
-    }
-}
